2e4b5ae45554fbbce697e6c37b1c9b7341371394
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2021-02-05 17:41:53 -0500

    button and label added

--- Departments/Form1.Designer.cs
+++ Departments/Form1.Designer.cs
@@ -29,13 +29,49 @@ namespace Departments
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loadFileButton = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loadFileButton
+            // 
+            this.loadFileButton.Location = new System.Drawing.Point(230, 142);
+            this.loadFileButton.Name = "loadFileButton";
+            this.loadFileButton.Size = new System.Drawing.Size(160, 54);
+            this.loadFileButton.TabIndex = 0;
+            this.loadFileButton.Text = "Load file";
+            this.loadFileButton.UseVisualStyleBackColor = true;
+            this.loadFileButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(183, 35);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(244, 25);
+            this.title.TabIndex = 1;
+            this.title.Text = "Department Database";
+            this.title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(615, 368);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.loadFileButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadFileButton;
+        private System.Windows.Forms.Label title;
     }
 }
 
